c9eedb2739baffba262248f6be3e71ccd9f4ebbb
pateelsrinathreddy <srinath.p99@gmail.com>
2024-10-24 13:06:19 +0530

    changes made in srinath branch

--- src/Testing.js
+++ src/Testing.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Testing() {
   return (
     <div>
-      <h1>Testing component modified</h1>
+      <h1>Testing component modified from srinath branch</h1>
     </div>
   )
 }
